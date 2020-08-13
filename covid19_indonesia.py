bdf583a0042fee0c98a3cff91d6e49001137e082
Andrean Stevanus <andrean.stevanus@yahoo.co.id>
2020-08-13 14:37:04 +0700

    Update covid19_indonesia.py

--- covid19_indonesia.py
+++ covid19_indonesia.py
@@ -1,5 +1,4 @@
 import json
-import re
 import pandas as pd
 import matplotlib.pyplot as plt
 #df = pd.read_json("https://data.covid19.go.id/public/api/update.json")
@@ -19,10 +18,10 @@ harian = df["update"].iloc[6]
 #Ubah dict data harian menjadi dataframe
 dataharian = pd.DataFrame.from_dict(harian)
 print(dataharian.columns)#Cetak daftar kolom
-dataharian = dataharian[["key_as_string","jumlah_meninggal", "jumlah_sembuh", "jumlah_positif", "jumlah_dirawat"]]
+dataharian = dataharian[["key_as_string","jumlah_meninggal", "jumlah_sembuh", "jumlah_positif", "jumlah_dirawat","jumlah_meninggal_kum", "jumlah_sembuh_kum", "jumlah_positif_kum", "jumlah_dirawat_kum"]]
 
 #Data n hari terakhir
-n = 10
+n = 157
 dataharian_n = dataharian.iloc[157-n:157]
 #Note: jumlah_meninggal + jumlah_sembuh + jumlah_dirawat = jumlah_positif
 #Ada tiga kemungkinan yang dapat terjadi pada orang yang positif: sembuh, belum sembuh(dirawat), meninggal
@@ -39,6 +38,10 @@ dataharian_n["jumlah_meninggal"] = valueformat(dataharian_n["jumlah_meninggal"])
 dataharian_n["jumlah_sembuh"] = valueformat(dataharian_n["jumlah_sembuh"])
 dataharian_n["jumlah_positif"] = valueformat(dataharian_n["jumlah_positif"])
 dataharian_n["jumlah_dirawat"] = valueformat(dataharian_n["jumlah_dirawat"])
+dataharian_n["jumlah_meninggal_kum"] = valueformat(dataharian_n["jumlah_meninggal_kum"])
+dataharian_n["jumlah_sembuh_kum"] = valueformat(dataharian_n["jumlah_sembuh_kum"])
+dataharian_n["jumlah_positif_kum"] = valueformat(dataharian_n["jumlah_positif_kum"])
+dataharian_n["jumlah_dirawat_kum"] = valueformat(dataharian_n["jumlah_dirawat_kum"])
 
 #Mengganti format tanggal 
 n=0
@@ -51,24 +54,30 @@ dataharian_n = dataharian_n.rename(columns={"key_as_string": "Tanggal"})
 #dataharian_n['Tanggal'] = pd.to_datetime(dataharian_n['Tanggal'])
 print(dataharian_n)
 
-#dataharian_n = dataharian_n.set_index('Tanggal')
+#Mengkopi data kolom ke variabel list
+def plotvalue(df_col,list_col):
+    for i in df_col:
+        list_col.append(i)
+        
 x_data = []
-for i in dataharian_n['Tanggal']:
-    x_data.append(i)
+plotvalue(dataharian_n['Tanggal'], x_data)
 y_data1 = []
-for i in dataharian_n['jumlah_meninggal']:
-    y_data1.append(i)
+plotvalue(dataharian_n['jumlah_meninggal'], y_data1)
 y_data2 = []
-for i in dataharian_n['jumlah_sembuh']:
-    y_data2.append(i)
+plotvalue(dataharian_n['jumlah_sembuh'], y_data2)
 y_data3 = []
-for i in dataharian_n['jumlah_positif']:
-    y_data3.append(i)
+plotvalue(dataharian_n['jumlah_positif'], y_data3)
 y_data4 = []
-for i in dataharian_n['jumlah_dirawat']:
-    y_data4.append(i)
-print(x_data)
-pd.to_datetime(x_data, format='%Y/%m/%d')
+plotvalue(dataharian_n['jumlah_dirawat'], y_data4)
+y_data1_kum = []
+plotvalue(dataharian_n['jumlah_meninggal_kum'], y_data1_kum)
+y_data2_kum = []
+plotvalue(dataharian_n['jumlah_sembuh_kum'], y_data2_kum)
+y_data3_kum = []
+plotvalue(dataharian_n['jumlah_positif_kum'], y_data3_kum)
+y_data4_kum = []
+plotvalue(dataharian_n['jumlah_dirawat_kum'], y_data4_kum)
+x_data = pd.to_datetime(x_data, format='%Y/%m/%d')
 
 #Grafik garis
 plot1 = plt.plot(x_data, y_data1, color = 'r',label = 'Jumlah Meninggal')
@@ -77,6 +86,18 @@ plot3 = plt.plot(x_data, y_data3, color = 'm',label = 'Jumlah Positif')
 plot4 = plt.plot(x_data, y_data4, color = 'y',label = 'Jumlah Dirawat')
 plt.xlabel("Tanggal")
 plt.ylabel("Jumlah Kematian")
-plt.title("Grafik Kasus COVID-19")
+plt.title("Grafik Laju Penambahan Kasus COVID-19")
 plt.legend()
 plt.show()
+
+#Grafik garis
+plot1 = plt.plot(x_data, y_data1_kum, color = 'r',label = 'Jumlah Meninggal')
+plot2 = plt.plot(x_data, y_data2_kum, color = 'b',label = 'Jumlah Sembuh')
+plot3 = plt.plot(x_data, y_data3_kum, color = 'm',label = 'Jumlah Positif')
+plot4 = plt.plot(x_data, y_data4_kum, color = 'y',label = 'Jumlah Dirawat')
+plt.xlabel("Tanggal")
+plt.ylabel("Jumlah")
+plt.title("Grafik Kasus COVID-19 Kumulatif")
+plt.legend()
+plt.show()
+
